45122aac0090e32f012164f0c2058e899cb93d26
Mario Yordanov <Mario.Yordanov@pds.nl>
2016-12-30 14:30:39 +0200

    Added the whole backtracking logic

---
+++ ConsoleApplication1/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    class Startup
+    {
+        
+        static void Main(string[] args)
+        {
+            int[,] table =
+            {
+                {0,4,0, 1,0,0 ,0,0,2 },
+                {6,0,0, 0,0,4 ,0,0,0 },
+                {0,0,5, 0,0,3 ,4,1,0 },
+
+                {4,0,0, 5,0,0 ,1,0,0 },
+                {0,0,8, 7,0,1 ,0,0,0 },
+                {7,0,0, 0,8,0 ,5,4,3 },
+
+                {1,0,9, 3,0,5 ,2,0,0 },
+                {0,0,2, 0,0,0 ,6,3,0 },
+                {0,3,0, 9,6,0 ,0,5,0 }
+            };
+            Console.WriteLine("SUDOKU TO SOLVE");
+            Console.WriteLine();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(" " + table[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Sudoku.SolveSudoku(table);
+            Sudoku.PrintSudoku(table);
+        }
+        
+    }
+}
---
+++ ConsoleApplication1/Sudoku.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    class Sudoku
+    {
+        public static bool SolveSudoku(int[,] table, int row = 0, int col = 0)
+        {
+            if (col == 9)
+            {
+                row++;
+                col = 0;
+
+                if (row == 9)
+                {
+                    return true;
+                }
+            }
+
+            if (table[row, col] > 0)
+            {
+                return SolveSudoku(table, row, col + 1);
+            }
+
+            for (int i = 1; i <= 9; ++i)
+            {
+
+                bool isOk = true;
+                for (int l = 0; l < 9; ++l)
+                {
+                    if (table[row, l] == i || table[l, col] == i ||
+                        table[row / 3 * 3 + l / 3, col / 3 * 3 + l % 3] == i)
+                    {
+                        isOk = false;
+                        break;
+                    }
+                }
+                if (!isOk) continue;
+
+                table[row, col] = i;
+                if (SolveSudoku(table, row, col + 1))
+                {
+                    return true;
+                }
+
+                table[row, col] = 0;
+            }
+            return false;
+        }
+
+        public static void PrintSudoku(int[,] table)
+        {
+            Console.WriteLine();
+            Console.WriteLine("SUDOKU SOLVED");
+            Console.WriteLine();
+            for (int i = 0; i < 9; ++i)
+            {
+                for (int j = 0; j < 9; ++j)
+                {
+                    Console.Write(" " + table[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
